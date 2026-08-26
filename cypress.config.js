4a534ce53caebb7cb6569cd814f5997567b89cf1
Luiz <teste@teste.com>
2026-08-25 22:50:32 -0300

    chore: aumenta defaultCommandTimeout do Cypress para 10s

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
     baseUrl: 'http://localhost:3000',
     env: {
       apiUrl: 'http://localhost:3001'
